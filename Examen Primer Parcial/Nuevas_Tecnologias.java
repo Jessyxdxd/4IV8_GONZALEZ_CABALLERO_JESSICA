e8aede518053aab9f056c12c2f87452984c4bfce
unknown <jgonzalezc2408@alumno.ipn.mx>
2026-03-23 20:01:15 -0600

    Examen

---
+++ Examen Primer Parcial/Nuevas_Tecnologias.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Nuevas_Tecnologias{
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+         
+        int opcion;
+
+        
+        while (true) { 
+            System.out.println("Seleccione una opción");
+            System.out.println("1. Toma de datos personales");
+            System.out.println("2. Calcular volumen de dos figuras");
+            System.out.println("3. Salir");
+        
+        switch (opcion){
+
+                case 1:  System.out.println("Ingrese sus datos");
+                case 2:  System.out.println("Programa para calcular volumen de dos figuras");
+                case 3: System.out.println("Salir");
+                default:System.out.println("");
+        }
+         }
+
+    }
+}
